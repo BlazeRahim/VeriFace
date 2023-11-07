7d777a7135f66e6af37cd1d5d2e74c0b64d4751c
JamalMydeen <jamessies123@gmail.com>
2023-11-07 16:30:37 +0530

    wdefvfr

--- client/src/components/Games.js
+++ client/src/components/Games.js
@@ -1,14 +1,17 @@
 import React,{useState ,useEffect , useRef}from 'react'
 import '../scss/games.scss'
 import {motion} from 'framer-motion';
-import Deep from '../images/Group 3.png'
+// import Deep from '../images/Group 5.png'
 import Logo from '../images/Logo.png'
 import scroll from '../utils/scroll';
 import CloseIcon from '@mui/icons-material/Close';
 import Bg from '../images/tp2.png'
 import Confetti from 'react-confetti'
 import ConfettiExplosion from 'react-confetti-explosion';
-import NoAudio from '../utils/no_audio';
+import Quiz from '../utils/quiz'
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
 const Games = () => {
   const [data,setdata] = useState([])
   const [flag,setflag] = useState(false)
@@ -20,6 +23,9 @@ const Games = () => {
   const [fast , setfast] = useState(false)
   const [finish , setfinish] = useState(false)
   const [score,setscore] = useState(0)
+  const [edition,setedition]=useState(-1)
+  console.log(edition)
+  const edarr = ["Celebrity Edition" , "Politician Edition" , "Random YT Video"]
  useEffect(()=>{
     if(flag===true){
       document.body.style.backgroundColor = "#605e5e";
@@ -46,7 +52,7 @@ useEffect(()=>{
     let name = "#id0"
     const ele = document.querySelector(name)
     ele.style.display="flex";
-    for(let i =1;i<NoAudio.length;i++){
+    for(let i =1;i<data.length;i++){
       let name = "#id"+i
       const ele = document.querySelector(name)
       ele.style.display="none";
@@ -55,15 +61,73 @@ useEffect(()=>{
     // eslint-disable-next-line
 },[rule])
 
+// let currindex=0
 
+const [ idx , setidx] = useState(0)
+const cards = document.querySelectorAll('.boxu');
+console.log("card" + cards.length)
+console.log("IDXX" + idx)
+function showCard() {
+    cards.forEach((card, index) => {
+      console.log(index-idx)
+        card.style.transform = `translateX(${(index - idx) * 120}%)`;
+    });
+}
 
+function nextCard() {
+  let currindex = idx + 1;
+  const ele = document.querySelector("#boxu"+currindex)
+  ele.style.transform = "translateX(0%)"
+  const el1 = document.querySelector("#boxu"+idx)
+  el1.style.animation="prev 1s ease"
+  console.log(currindex)
+  setidx(currindex)
+  // showCard();
+}
+function prevCard() {
+  let currindex = (idx - 1);
+  const ele = document.querySelector("#boxu"+currindex)
+  ele.style.transform = "translateX(0%)"
+  const el1 = document.querySelector("#boxu"+idx)
+  el1.style.animation="trans 1s ease"
+  setidx(currindex)
+  // showCard();
+}
+showCard();
   return ( 
     <div className="games" ref={game}>
        <motion.h2 initial={{ opacity: 0, x: -150 }}
         viewport={{ once: true }}
-        whileInView={{ opacity: 1, x: 0 }} transition={{duration:0.7 , type:"Spring" , bounce:0.4}}>Fun Time Games</motion.h2>
+        whileInView={{ opacity: 1, x: 0 }} transition={{duration:0.7 , type:"Spring" , bounce:0.4}}>DeepFake challenge</motion.h2>
         <div className="play">
-          <div className="box">
+          {(idx > 0) && <ArrowBackIosNewIcon className='arrow' onClick={(e)=>{
+            e.preventDefault()
+            prevCard()
+          }}/>}
+          <div className="card">
+          {Quiz.map((item,idx)=>(
+          <div className="boxu" id={`boxu${idx}`} key={idx}>
+            <img src={item.cover} alt="ef" />
+          <div className="theory">
+          <p>{item.heading} <span>{item.context}.</span></p>
+          <p>{item.focus}.</p>
+          <button onClick={(e)=>{
+            e.preventDefault()
+            setflag(true)
+            setrule(true) 
+            setdata(item.data) 
+            setedition(item.edition)  
+            setchecked(false)
+          }}>Start</button>
+          </div>
+              </div>
+          ))}
+          </div>
+           {(idx !== Quiz.length-1) && <ArrowForwardIosIcon className='arrow' onClick={(e)=>{
+            e.preventDefault()
+            nextCard()
+          }}/>}
+          {/* <div className="box">
           <img src={Deep} alt="ef" />
           <div className="theory">
           <p>Test your skills and see if you can spot the difference between real videos and deepfake creations <span>without audio.</span></p>
@@ -72,11 +136,12 @@ useEffect(()=>{
             e.preventDefault()
             setflag(true)
             setrule(true) 
-            setdata(NoAudio)   
+            setdata(NoAudio) 
+            setedition(0)  
             setchecked(false)
           }}>Start</button>
           </div>
-          </div>
+          </div> */}
         </div>
         {flag===true && (
           <div className="cover">
@@ -93,6 +158,8 @@ useEffect(()=>{
             )}
           <div className="up">
                    <img src={Logo} alt="asd" />
+                   <h1>DeepFake Challenge - <span>{edarr[0]}</span> 
+                   </h1>
                    <CloseIcon className='icon' onClick={(e)=>{
                     e.preventDefault()
                     document.body.style.backgroundColor = "white";
@@ -134,7 +201,10 @@ useEffect(()=>{
                   <div className="appear" initial={{ scale:0}}
                   viewport={{ once: true }}
                   whileInView={{ opacity: 1, scale: 1 }} transition={{ duration: 4 , type:"Spring" , bounce:0.9  }}  >
+                    <div className="ans">
                   <h1>The video was DeepFake edit , Here is the Original video.</h1>
+                  
+                  </div>
               <video loop autoPlay controls>
               <source src={item.real} type="video/mp4" />
               Your browser does not support the video tag.
@@ -222,12 +292,11 @@ useEffect(()=>{
                     <div className="finish">
                       <div className="score">
                         <h1>Your Score :</h1>
-                        <h3>{score}/{data.length} correct guess    |    {data.length - score} Wrong guess</h3>
+                        <h3>{score}/{data.length} correct guess</h3>
                       </div>
                       <div className="more">
                         <h1>Similar Challenges -</h1>
                         <div className="boxes">
-                          <div className="box"></div>
                           <div className="box"></div>
                           <div className="box"></div>
                         </div>
